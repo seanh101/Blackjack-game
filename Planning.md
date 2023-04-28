1565912b1a7526f14caf6d9447e318303df47814
seanh101 <seanharrington4189@gmail.com>
2023-04-28 00:19:00 -0400

    README complete, organize code, make sure DOM elements are cached

---
+++ Planning.md
@@ -0,0 +1,123 @@
+# Planning for Blackjack
+
+## Analyze the app's functionality
+
+As a user...
+
+- I want to be able to play against the dealer, because that is how Blackjack is played.
+- I want to be dealt two cards initially, and the dealer to also be dealt two cards, because that is how Blackjack starts.
+- I want to see the value of my cards and one of the dealer's cards, but not the other one, because that is how Blackjack works.
+- I want to be able to choose whether to hit (get another card) or stand (keep my current hand), because that is how I can try to beat the dealer's hand.
+- I want to see the value of my hand and the dealer's hand once I stand, because that is how I can determine who wins.
+- I want the dealer to automatically hit until they have a score of 17 or more, because that is how the dealer's turn works in Blackjack.
+- I want to know who won or if it was a push, and see the result displayed on the screen, because that is how I can keep track of the game's outcome.
+- I want to be able to play again and reset my chip amount, because that is how I can continue playing and trying to win.
+- I want to be able to wager a set amount each hand
+
+If there is time to add more features, the following could be considered:
+
+- A "flip" effect for the cards
+- Sound to play when you win
+- Splitting the cards
+- Doubling down
+
+
+## Think about the overall design (look & feel) of the app
+
+- CSS card library
+
+## Wireframe the UI
+
+<img src="./Imgs/blackjack_wireframe3.jpeg"/>
+
+- 2 starting cards for dealer and player.
+- 1 extra facedown card shown for deck
+- Hit stay and wager buttons located at bottom
+- Chip amount in box on right
+
+## Pseudocode
+
+1) Define required constants
+    1) cardValues - an object that holds the point value of each card
+    2) startingChips 
+
+2) Define variables used to track the state of the game
+    1) deck - an array of 52 cards
+    2) playerHand - an array of cards in the player's hand
+    3) dealerHand - an array of cards in the dealer's hand
+    4) playerScore - the total point value of the player's hand
+    5) dealerScore - the total point value of the dealer's hand
+    6) chips - the number of chips the player has
+
+3) Cache DOM elements
+    1) Message place
+    2) New game button
+    3) hit button
+    4) stay button
+    5) bet button
+
+
+4) Upon loading the app should:
+    1) Initialize the state variables
+        - Create a deck with a randomly ordered array of 52 cards
+        - Shuffle the deck
+        - Deal two cards to the player and two cards to the dealer
+        - Calculate the player's score
+        - Calculate the dealer's score
+        - Set the number of chips to the starting amount
+    2) Render those values to the page
+        - Render the game screen, should render 2 cards face up for the player, and 1 & 1 for the dealer
+        - Render  win/lose message after each hand
+        - Do not render new game button on first view
+        - Render the hit, stay, and bet buttons
+
+ 5) Handle a player clicking the hit button
+    1) Add a card to the player's hand
+    2) Calculate the new player score
+    3) If the player's score is over 21, end the game and declare the dealer as the winner
+    4) Render the game screen
+
+6) Handle a player clicking the stay button
+    1) Show the dealer's facedown card
+    2) While the dealer's score is less than 17, draw another card for the dealer
+    3) Calculate the new dealer score
+    4) Determine the winner and adjust the chips accordingly
+    5) Render the game screen
+
+7) Handle a player clicking the bet button      
+    1) Prompt the player to enter their bet amount
+    2) Subtract the bet amount from the number of chips
+    3) Render the game screen
+
+
+ 8) Handle a player clicking the new game button
+    1) Reset the state variables
+    2) Render the game screen
+
+9) Check for a winner or loser
+    1) If the player's score is over 21, the dealer wins
+    2) If the dealer's score is over 21, the player wins
+    3) If the player's score is higher than the dealer's score, the player wins
+    4) If the dealer's score is higher than the player's score, the dealer wins
+    5) If the scores are tied, it's a push and the chips are returned to the player
+    6) Adjust the chips accordingly
+
+## Identify the application's state (data)
+
+let deck
+
+let playerHand
+
+let dealerHand
+
+let playerScore
+
+let dealerScore
+
+let chips
+
+let betAmount
+
+let gameOver
+
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// Constants
+
+const suits = ['spades', 'clubs', 'diamonds', 'hearts'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
 // Initialize variables
 let deck;
 let dealerHand = [];
@@ -11,18 +16,32 @@ let playerCanHit = true;
 let chipAmount = 500;
 let betAmount = 0;
 
-// Constants
+// Cached DOM elements
+const dealerCardContainer = document.querySelector('#dealer-cards');
+const playerCardContainer = document.querySelector('#player-cards');
+const hitButton = document.getElementById('hit');
+const stayButton = document.getElementById('stay');
+const playAgainBtn = document.getElementById("play-again");
+const dealerScoreDisplay = document.getElementById('dealer-score');
+const playerScoreDisplay = document.getElementById('player-score');
+const resultsDisplay = document.getElementById('results');
+const chipAmountDisplay = document.getElementById('chip-amount');
 
-const suits = ['spades', 'clubs', 'diamonds', 'hearts'];
-const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+// event listeners
+hitButton.addEventListener("click", hit);
+stayButton.addEventListener("click", stay);
+playAgainBtn.addEventListener("click", playAgain);
+hitButton.addEventListener("click", updateChips);
 
-// Call functions on page load
+// Load game
 window.onload = function() {
     createDeck();
     shuffleDeck();
     dealCards();
     
 }
+
+// Functions
 
 function createDeck() {
     deck = [];
@@ -44,37 +63,54 @@ function shuffleDeck() {
     }
 }
 
+
+
 // Start the game
 function dealCards() {
     
-       // clear any existing cards
-       const dealerCardContainer = document.querySelector('#dealer-cards');
-       while (dealerCardContainer.firstElementChild) {
-           dealerCardContainer.firstElementChild.remove();
+    // clear any existing cards
+       
+    while (dealerCardContainer.firstElementChild) {
+        dealerCardContainer.firstElementChild.remove();
        }
+       
    
        // deal one card face-down
-       let faceDownCardImg = document.createElement("img");
-       let faceDownCard = deck.pop();
-       let faceDownSuit = faceDownCard.split("-")[1];
-       let faceDownRank = faceDownCard.split("-")[0];
-       faceDownCardImg.src = `./css/card-library/images/backs/red.svg`;
-       dealerScore += getRank(faceDownCard);
-       dealerAces += checkAce(faceDownCard);
-       dealerCardContainer.append(faceDownCardImg);
+       
+    let faceDownCardImg = document.createElement("img");
+    let faceDownCard = deck.pop();
+       
+    faceDownCardImg.src = `./css/card-library/images/backs/red.svg`;
+    dealerScore += getRank(faceDownCard);
+    dealerAces += checkAce(faceDownCard);
+    dealerCardContainer.append(faceDownCardImg);
    
-       // deal one card face-up
-       let faceUpCardImg = document.createElement("img");
-       let faceUpCard = deck.pop();
-       let faceUpSuit = faceUpCard.split("-")[1];
-       let faceUpRank = faceUpCard.split("-")[0];
-       faceUpCardImg.src = `./css/card-library/images/${faceUpSuit}-r${faceUpRank}.svg`;
-       dealerScore += getRank(faceUpCard);
-       dealerAces += checkAce(faceUpCard);
-       dealerCardContainer.append(faceUpCardImg);
-   
-       // set dealer score
-       console.log(dealerScore);
+    // deal one card face-up
+
+     
+    let faceUpCardImg = document.createElement("img");
+    let faceUpCard = deck.pop();
+    let faceUpSuit = faceUpCard.split("-")[1];
+    let faceUpRank = faceUpCard.split("-")[0];
+    faceUpCardImg.src = `./css/card-library/images/${faceUpSuit}-r${faceUpRank}.svg`;
+    dealerScore += getRank(faceUpCard);
+    dealerAces += checkAce(faceUpCard);
+    dealerCardContainer.append(faceUpCardImg);
+       
+    while (dealerScore < 17) {
+        let dealerHitCardImg = document.createElement("img");
+        let dealerHitCard = deck.pop();
+        let dealerHitSuit = dealerHitCard.split("-")[1];
+        let dealerHitRank = dealerHitCard.split("-")[0];
+        dealerHitCardImg.src = `./css/card-library/images/${dealerHitSuit}-r${dealerHitRank}.svg`;
+        dealerScore += getRank(dealerHitCard);
+        dealerAces += checkAce(dealerHitCard);
+        dealerCardContainer.append(dealerHitCardImg);
+    }
+      
+
+    // set dealer score
+    console.log(dealerScore);
 
     // set player cards and score
     for (let i = 0; i < 2; i++) {
@@ -85,17 +121,12 @@ function dealCards() {
         cardImg.src = `./css/card-library/images/${suit}-r${rank}.svg`;
         playerScore += getRank(card);
         playerAces += checkAce(card);
-        document.getElementById("player-cards").append(cardImg);
+        playerCardContainer.append(cardImg);
     }
-
     console.log(playerScore)
-    // event listeners
-    document.getElementById("hit").addEventListener("click", hit);
-    document.getElementById("stay").addEventListener("click", stay);
-
-    
 }
 
+// Player Hit Function
 function hit() {
     if (!playerCanHit) {
         return;
@@ -107,65 +138,66 @@ function hit() {
     cardImg.src = `./css/card-library/images/${suit}-r${rank}.svg`;
     playerScore += getRank(card);
     playerAces += checkAce(card);
-    document.getElementById("player-cards").append(cardImg);
+  playerCardContainer.append(cardImg);
+    while (dealerScore < 17) {
+        let dealerHitCardImg = document.createElement("img");
+        let dealerHitCard = deck.pop();
+        let dealerHitSuit = dealerHitCard.split("-")[1];
+        let dealerHitRank = dealerHitCard.split("-")[0];
+        dealerHitCardImg.src = `./css/card-library/images/${dealerHitSuit}-r${dealerHitRank}.svg`;
+        dealerScore += getRank(dealerHitCard);
+        dealerAces += checkAce(dealerHitCard);
+        dealerCardContainer.append(dealerHitCardImg);
+    }
 
+    
     if (reduceAce(playerScore, playerAces) > 21) {
         playerCanHit = false;
+        message = "YOU LOSE!";
+        chipAmount -= betAmount;
     }
+  
     let message = "";
-        if (playerScore > 21) {
-            message = "YOU LOSE!";
-          
-        }
-    
         
-        document.getElementById("dealer-score").innerText = dealerScore;
-        document.getElementById("player-score").innerText = playerScore;
-        document.getElementById("results").innerText = message;
+    dealerScoreDisplay.innerText = dealerScore;
+    playerScoreDisplay.innerText = playerScore;
+    resultsDisplay.innerText = message;
     
     }
 
-console.log(faceDown);
 
-    function stay() {
-        dealerScore = reduceAce(dealerScore, dealerAces);
-        playerScore = reduceAce(playerScore, playerAces);
-    
-        playerCanHit = false;
-        
-        // reveal the face-down card
-        let faceDownCardImg = document.createElement("img");
-        let faceDownCard = deck.pop();
-       let faceDownSuit = faceDownCard.split("-")[1];
-       let faceDownRank = faceDownCard.split("-")[0];
-       faceDownCardImg.src = `./css/card-library/images/backs/red.svg`;
-        let message = "";
-        if (playerScore > 21) {
-            message = "YOU LOSE!";
-          
-        }
-        else if (dealerScore > 21) {
-            message = "YOU WIN";
-            
-        }
-        else if (playerScore === dealerScore) {
-            message = "PUSH";
-            
-        }
-        else if (playerScore > dealerScore) {
-            message = "YOU WIN!";
-            
-        }
-        else if (playerScore < dealerScore) {
-            message = "YOU LOSE!";
-            
-        }
-        
-        document.getElementById("dealer-score").innerText = dealerScore;
-        document.getElementById("player-score").innerText = playerScore;
-        document.getElementById("results").innerText = message;
-    
+
+function stay() {
+    playerCanHit = false;
+
+    while (dealerScore < 17) {
+        let dealerHitCardImg = document.createElement("img");
+        let dealerHitCard = deck.pop();
+        let dealerHitSuit = dealerHitCard.split("-")[1];
+        let dealerHitRank = dealerHitCard.split("-")[0];
+        dealerHitCardImg.src = `./css/card-library/images/${dealerHitSuit}-r${dealerHitRank}.svg`;
+        dealerScore += getRank(dealerHitCard);
+        dealerAces += checkAce(dealerHitCard);
+        dealerCardContainer.append(dealerHitCardImg);
     }
+
+    let playerScoreDisplayValue = reduceAce(playerScore, playerAces);
+    let dealerScoreDisplayValue = reduceAce(dealerScore, dealerAces);
+
+    playerScoreDisplay.textContent = `Player: ${playerScoreDisplayValue}`;
+    dealerScoreDisplay.textContent = `Dealer: ${dealerScoreDisplayValue}`;
+
+    if (dealerScoreDisplayValue > 21 || playerScoreDisplayValue > dealerScoreDisplayValue) {
+        resultsDisplay.textContent = 'You win!';
+        chipAmount += betAmount;
+    } else if (dealerScoreDisplayValue > playerScoreDisplayValue) {
+        resultsDisplay.textContent = 'You lose!';
+        chipAmount -= betAmount;
+    } else {
+        resultsDisplay.textContent = 'Push!';
+    }
+    betAmount = 0;
+}
     
 
 
@@ -200,60 +232,48 @@ function reduceAce(playerScore, playerAces) {
     return playerScore;
 }
 
-// Chips
-let playerChips = chipAmount;
-// Add event listener for bet button
-document.getElementById("bet").addEventListener("click", function() {
-    // Get bet amount entered by user
-    let betAmount = document.getElementById("bet-input").value;
-  
-  
-     // Convert bet amount to integer
-     betAmount = parseInt(betAmount);
 
-     // Subtract bet amount from player's chips and update display
-     playerChips -= betAmount;
-     document.getElementById("chips").innerHTML = `Chips: ${playerChips}`;
- 
-     // Start the game
-     let gameResult = dealCards();
- 
-     // Update player's chips based on game outcome
-     if (gameResult === "win") {
-       playerChips += betAmount * 2; // double the bet amount for a win
-       document.getElementById("results").innerHTML = "You win!";
-     } else {
-       document.getElementById("results").innerHTML = "You lose!";
-     }
-     document.getElementById("chips").innerHTML = `Chips: ${playerChips}`;
-   });
- 
-   function updateChipsDisplay() {
-     document.getElementById("chips").innerText = playerChips;
-   }
- 
-   function playAgain() {
-    // reset game variables
+function playAgain() {
+    resultsDisplay.textContent = "";
+    dealerScoreDisplay.textContent = "";
+    playerScoreDisplay.textContent = "";
+    while (dealerCardContainer.firstElementChild) {
+        dealerCardContainer.firstElementChild.remove();
+    }
+    while (playerCardContainer.firstElementChild) {
+        playerCardContainer.firstElementChild.remove();
+    }
+    deck = [];
     dealerHand = [];
     playerHand = [];
     dealerScore = 0;
     playerScore = 0;
     dealerAces = 0;
     playerAces = 0;
-    faceDown = null;
     playerCanHit = true;
-
-    // reset card images and result message
-    document.getElementById("dealer-cards").innerHTML = "";
-    document.getElementById("player-cards").innerHTML = "";
-    document.getElementById("results").innerText = "";
-
-    // shuffle and deal cards again
     createDeck();
     shuffleDeck();
     dealCards();
+    chipAmountDisplay.textContent = chipAmount;
 }
 
-document.getElementById("play-again").addEventListener("click", playAgain);
 
 
+
+
+function updateChips(betAmount, isWin) {
+    const betInput = parseInt(document.getElementById("bet-input").value);
+    const chipAmount = parseInt(document.getElementById("chip-amount").innerText);
+    
+    if (isWin) {
+      const winnings = betAmount * 2;
+      chipAmount.textContent = Number(chipAmount.textContent) + winnings;
+    } else {
+      chipAmount.textContent = Number(chipAmount.textContent) - betAmount;
+    }
+    
+    betInput.value = ""; // reset bet input
+  }
+  hitButton.addEventListener("click", updateChips);
+
+  
